a83da91a5d2a57557c90911207c025ba48bc81ff
Moiz Rizvi <moizrizvi@gmail.com>
2016-08-29 01:23:12 -0500

    Boilerplate code added

---
+++ src/main/kotlin/com/moizrizvi/fullbarrel/main.kt
@@ -0,0 +1,6 @@
+package com.moizrizvi.fullbarrel
+
+fun main(args: Array<String>) {
+    println("Hello World")
+}
+
---
+++ src/test/kotlin/com/moizrizvi/fullbarrel/stub_test.kt
@@ -0,0 +1,18 @@
+package com.moizrizvi.fullbarrel
+
+import org.junit.*
+import kotlin.test.*
+
+class StubTest {
+    @Before fun before() {
+
+    }
+
+    @Test fun stub() {
+        assertTrue(false)
+    }
+
+    @After fun after() {
+    }
+}
+
